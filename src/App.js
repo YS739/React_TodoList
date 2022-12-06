403cd2a6324f6660a781e50da95e4e11b85835dc
YS739 <suk921@gmail.com>
2022-12-06 18:36:51 +0900

    doom2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-
-=======
 import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/header";
@@ -10,4 +7,3 @@ const addToDo = () => {
 };
 
 export default addToDo;
->>>>>>> parent of 79d9338 (done except split isdone true or false)
