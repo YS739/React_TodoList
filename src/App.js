c2db27555c508f4af97267f93329889e54881aa6
YS739 <suk921@gmail.com>
2022-12-06 18:36:19 +0900

    doom

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
+<<<<<<< HEAD
 
+=======
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/header";
+
+const addToDo = () => {
+  return <div></div>;
+};
+
+export default addToDo;
+>>>>>>> parent of 79d9338 (done except split isdone true or false)
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import "../App.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <span>My To Do List</span>
+      <span>React</span>
+    </div>
+  );
+};
+
+export default Header;
